255d89b4afc06d3be7d2a2c51965d1a48278b2c5
SERGIO DANIEL LÓPEZ BARRIENTOS <sergiodaniel007@gmail.com>
2026-02-11 12:37:39 -0600

    Clase FileReader

---
+++ src/main/java/FileReader.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FileReader {
+    public static String readFile(String filePath) throws IOException {
+        Path path = Paths.get(filePath);
+        return Files.readString(path);
+    }
+}
