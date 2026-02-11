7ece8a0ba1ccf9ab41e6fbd99f9a3b55aad0ba24
SERGIO DANIEL LÓPEZ BARRIENTOS <sergiodaniel007@gmail.com>
2026-02-11 12:57:06 -0600

    OP_1...OP_16

--- src/main/java/Opcodes.java
+++ src/main/java/Opcodes.java
@@ -2,6 +2,74 @@ public class Opcodes<T> {
 
     private Stack<T> stack = new Stack<>();
 
+    public void OP_0() {
+        stack.push((T) Integer.valueOf(0));
+    }
+
+    public void OP_1() {
+        stack.push((T) Integer.valueOf(1));
+    }
+
+    public void OP_2() {
+        stack.push((T) Integer.valueOf(2));
+    }
+
+    public void OP_3() {
+        stack.push((T) Integer.valueOf(3));
+    }
+
+    public void OP_4() {
+        stack.push((T) Integer.valueOf(4));
+    }
+
+    public void OP_5() {
+        stack.push((T) Integer.valueOf(5));
+    }
+
+    public void OP_6() {
+        stack.push((T) Integer.valueOf(6));
+    }
+
+    public void OP_7() {
+        stack.push((T) Integer.valueOf(7));
+    }
+
+    public void OP_8() {
+        stack.push((T) Integer.valueOf(8));
+    }
+
+    public void OP_9() {
+        stack.push((T) Integer.valueOf(9));
+    }
+
+    public void OP_10() {
+        stack.push((T) Integer.valueOf(10));
+    }
+
+    public void OP_11() {
+        stack.push((T) Integer.valueOf(11));
+    }
+
+    public void OP_12() {
+        stack.push((T) Integer.valueOf(12));
+    }
+
+    public void OP_13() {
+        stack.push((T) Integer.valueOf(13));
+    }
+
+    public void OP_14() {
+        stack.push((T) Integer.valueOf(14));
+    }
+
+    public void OP_15() {
+        stack.push((T) Integer.valueOf(15));
+    }
+
+    public void OP_16() {
+        stack.push((T) Integer.valueOf(16));
+    }
+
     public void PUSH_DATA() {
         // Implementation for pushing data onto the stack
     }
@@ -34,4 +102,5 @@ public class Opcodes<T> {
             return false;
         }
     }
+
 }
