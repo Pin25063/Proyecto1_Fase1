8cd9a57bc6b68d2a036f4b0992bbd4467d5b0097
DarkFox90 <carlospozuelos3@gmail.com>
2026-02-13 19:53:19 -0600

    metodo helper par OP_CHECKSIG

--- src/main/java/Opcodes.java
+++ src/main/java/Opcodes.java
@@ -182,6 +182,14 @@ public class Opcodes<T> {
         }
     }
 
+    private String bytesToHex(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (byte b : bytes) {
+            sb.append(String.format("%02X", b));
+        }
+        return sb.toString();
+    }
+
     public boolean execute(String token) {
         Runnable operation = opcodeMap.get(token);
         if (operation != null) {
